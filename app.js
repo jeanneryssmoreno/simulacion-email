038d0905167099215a1c98ee2bbe6f4b4ab3860f
jeanneryssmoreno <morenojeannrys@gmail.com>
2025-10-20 01:14:32 -0400

    seleccionar loe elemntos del html

---
+++ app.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    //seleccionar loe elementos de la interfaz
+
+    const inputEmail = document.querySelector('#email');
+    const asunto = document.querySelector('#asunto');
+    const mensaje = document.querySelector('#mensaje');
+})
